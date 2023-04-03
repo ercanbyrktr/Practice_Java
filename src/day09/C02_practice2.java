3a0bab6d006a887b94be32bd967554d927d60291
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-04-03 18:07:13 +0300

    03/04/2023

---
+++ src/day09/C02_practice2.java
@@ -0,0 +1,26 @@
+package day09;
+
+public class C02_practice2 {
+    public static void main(String[] args) {
+        /**
+         * 01234   43210
+         * kayk,==kayak      anna, deed , Madam im adam
+         * kayak.equal(kayak)
+         * kayak.contain(kayak)
+         *
+         *
+         */
+        palindrome("kursad");
+    }
+
+    public static void palindrome(String text){
+        String reverse ="";
+        for (int i = text.length()-1; i >=0 ; i--) {
+            reverse += text.charAt(i);
+        }
+        if (text.equalsIgnoreCase(reverse)){
+            System.out.println(text +" is a Palindrome ");
+        }else
+            System.out.println(text +" is NOT a Palindrome ");
+    }
+}
---
+++ src/day09/C03_practice3.java
@@ -0,0 +1,7 @@
+package day09;
+
+public class C03_practice3 {
+    public static void main(String[] args) {
+
+    }
+}
