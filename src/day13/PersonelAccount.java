b4b6d8fc468b5e2c882fd892bcfa5d3b469c78a2
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-04-17 12:06:35 +0300

    17/04/2023

---
+++ src/day13/PersonelAccount.java
@@ -0,0 +1,14 @@
+package day13;
+
+public class PersonelAccount {
+    public static void main(String[] args) {
+
+        Bank person1 = new Bank();
+
+        person1.setAccountDetail("Amr Mohamed","Saving",65543364,15501.12);
+        person1.accountPurchase(550.50,2);
+        person1.getBalance();
+        person1.accountDeposit(5025.25);
+        person1.getBalance();
+    }
+}
