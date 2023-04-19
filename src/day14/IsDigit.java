a516daacc2bec2a9cd27e1640637304756d73b6d
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-04-19 12:41:07 +0300

    19/04/2023

---
+++ src/day14/IsDigit.java
@@ -0,0 +1,12 @@
+package day14;
+
+public class IsDigit {
+    public static void main(String[] args) {
+
+        String edition =" Programming with this updated guide with the latest features, 2nd Edition";
+        String str ="";
+        for (int i = 0; i <edition.length() ; i++) {
+            str += edition.charAt(i);
+            System.out.println(Character.isDigit(str.charAt(i)));
+    }
+}
