19351b19bfe3c17f8afcf836e111ce4e732613a4
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-05-29 15:43:40 +0300

    29/05/2023

---
+++ src/day19/Student.java
@@ -0,0 +1,34 @@
+package day19;
+
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private int year;
+
+
+    public Student(int studentId, String firstName, String lastName, int year) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public String getInfo() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
