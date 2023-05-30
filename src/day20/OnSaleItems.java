6f0a25c47cd38e230871fce1c4193dc2b4e45958
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-05-30 18:49:59 +0300

    30/05/2023

---
+++ src/day20/OnSaleItems.java
@@ -0,0 +1,27 @@
+package day20;
+
+public class OnSaleItems extends Items{
+    private double discount;
+
+    public OnSaleItems(String name, int catalogId, int quantity, double price, double discount) {
+        super(name, catalogId, quantity, price);
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "OnSaleItems{" +
+                "\ndiscount=" + discount +"%"+
+                "\nItem name"+ getName()+
+                "\nItem price" + getPrice()+"$"+
+                '}';
+    }
+}
---
+++ src/day20/Test.java
@@ -0,0 +1,17 @@
+package day20;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        Items it1 = new Items("Iphone",0001,1,1360.00);
+        OnSaleItems onSaleItem1 = new OnSaleItems("Iphone13x",0002,2,1402.00,10);
+        Action act = new Action();
+        act.addNormalItems(it1);
+        act.addOnsaleItem(onSaleItem1);
+        act.saleItemById(0001);
+        System.out.println(act.getByCatalogId(0001));
+
+
+    }
+}
